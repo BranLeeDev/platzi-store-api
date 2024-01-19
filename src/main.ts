7385f458bfcb65fa3572a47c98e099b76445de1a
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 09:09:04 -0500

    feat: Set global prefix to 'api/v1' and configured Swagger documentation
    - Utilized 'setGlobalPrefix' function for 'app' constant.
    - Added Swagger documentation with a title, description, version, and endpoint at 'api/v1/docs'

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 // NestJS modules
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // Third-party libraries
 import helmet from 'helmet';
@@ -17,6 +18,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
+
+  app.setGlobalPrefix('api/v1');
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Platzi Store API Documentation')
+    .setDescription(
+      'REST API built with NestJS, Docker, PostgreSQL and TypeORM',
+    )
+    .setVersion('0.0.1')
+    .build();
+
+  const documentSwagger = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api/v1/docs', app, documentSwagger);
 
   await app.listen(PORT);
 }
