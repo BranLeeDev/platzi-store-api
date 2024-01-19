67e7c77246e2917c944bee68f3d7eb05c95cc3c7
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 12:05:47 -0500

    feat: Updated 'brand.entity' in the 'products' module
    - Added the '@Entity({name: 'brands'})' decorator to the exportable 'Brand' class.
    - Applied the '@PrimaryGeneratedColumn()' decorator to the 'id' attribute.
    - Utilized the '@Column({type: 'varchar', length: 30})' decorator for the 'name' attribute.
    - Employed the '@Column({type: 'varchar', length: 255})' decorator for the 'description' attribute.
    - Imported decorators from the 'typeorm' library.

--- src/modules/products/entities/brand.entity.ts
+++ src/modules/products/entities/brand.entity.ts
@@ -1,5 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'brands' })
 export class Brand {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 30 })
   name: string;
+
+  @Column({ type: 'varchar', length: 255 })
   description: string;
 }
