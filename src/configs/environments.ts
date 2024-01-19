266dfdb2c41c341ea1a166b81631437129f8cfd3
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 07:17:40 -0500

    feat: Added 'environments.ts' file in 'configs' folder
    Created the exportable constant 'environments' of type 'EnvironmentFiles'. The 'EnvironmentFiles' interface was imported from 'types.d.ts'

---
+++ src/configs/environments.ts
@@ -0,0 +1,7 @@
+import { EnvironmentFiles } from 'src/types';
+
+export const environments: EnvironmentFiles = {
+  development: '.env.development.local',
+  test: '.env.test.local',
+  production: '.env',
+};
