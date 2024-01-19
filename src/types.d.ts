01bdfd986964d5a7cb6da9c447d733442eb3575e
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 06:54:32 -0500

    feat: Created exportable 'Config' interface
    Defined 'Config' interface with 'env' of type 'environments', 'port' of type 'number', and 'isPro' of type 'boolean' in 'types.d.ts' within the 'src' folder

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,11 @@
 export type environments = 'development' | 'test' | 'production';
 
+export interface Config {
+  env: environments;
+  port: number;
+  isPro: boolean;
+}
+
 export interface EnvironmentFiles {
   development: '.env.development.local';
   test: '.env.test.local';
