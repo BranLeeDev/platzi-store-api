f01fe15b11ef153b33b56bf81a2804d0ab11de8a
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 10:37:39 -0500

    feat: Configured 'TypeOrmModule' with 'forRootAsync' method
    - Imported 'TypeOrmModule' class from '@nestjs/typeorm'.
    - Configured the 'TypeOrmModule' in the '@Module' decorator's 'imports' property.
    - Used 'forRootAsync' method, passing an object with 'inject' property as [registers.KEY].
    - Imported 'registers' from 'registers.ts'.
    - Added 'useFactory' property with an arrow function, creating 'DATABASE_URL' variable based on environment.
    - Arrow function returns an object with properties: 'type' as 'postgres', 'url' as 'DATABASE_URL', 'synchronize' as true, and 'autoLoadEntities' as true.
    - Configuration done in 'database.module.ts' within the 'database' module.

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,5 +1,39 @@
+// NestJS modules
 import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Config imports
+import { config } from 'src/configs/config';
+import registers from 'src/configs/registers';
 
 @Global()
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [registers.KEY],
+      useFactory: (registerService: ConfigType<typeof registers>) => {
+        let DATABASE_URL: string;
+        if (config.env === 'production') {
+          DATABASE_URL = registerService.db.postgres.dbUrl;
+        } else {
+          const { user, password, host, port, name } =
+            registerService.db.postgres;
+
+          const USER = encodeURIComponent(user);
+          const PASSWORD = encodeURIComponent(password);
+
+          DATABASE_URL = `postgresql://${USER}:${PASSWORD}@${host}:${port}/${name}`;
+        }
+
+        return {
+          type: 'postgres',
+          url: DATABASE_URL,
+          synchronize: true,
+          autoLoadEntities: true,
+        };
+      },
+    }),
+  ],
+})
 export class DatabaseModule {}
