b246ddc6e1a2388305205e521eb7dcb19217b7ea
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 06:11:16 -0500

    feat: ThrottlerModule configuration in app.module.ts
    Imported ThrottlerModule, configured 'options' in forRoot with TTL 60000 and limit 7 in imports. Added ThrottlerGuard as APP_GUARD in providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 7,
+      },
+    ]),
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
