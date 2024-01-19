a7759503953a9783085a89037c0d34e0247b5539
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 07:45:02 -0500

    feat: Added 'validationSchema' to ConfigModule in app.module.ts
    Configured 'ConfigModule' in app.module.ts with 'validationSchema' parameter to validate 'NODE_ENV' and 'PORT' environment variables using 'joi' library

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
+// Third-party libraries
+import * as Joi from 'joi';
+
 // Config imports
 import { config } from './configs/config';
 import { environments } from './configs/environments';
@@ -21,6 +24,10 @@ import registers from './configs/registers';
       envFilePath: environments[config.env],
       load: [registers],
       isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.valid('development', 'test', 'production').required(),
+        PORT: Joi.number().integer().positive().min(1000),
+      }),
     }),
   ],
   controllers: [],
