45f914b09917a275a7f198fa71b230dcd3e5d219
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 14:51:44 -0500

    docs: Add Swagger annotations for 'brands' controller and create-dto

--- src/modules/products/controllers/brands/brands.controller.ts
+++ src/modules/products/controllers/brands/brands.controller.ts
@@ -9,6 +9,7 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ApiBody, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 // Service imports
 import { BrandsService } from '../../services/brands/brands.service';
@@ -17,26 +18,50 @@ import { BrandsService } from '../../services/brands/brands.service';
 import { UpdateBrandDto } from '../../dtos/brands/update-brand.dto';
 import { CreateBrandDto } from '../../dtos/brands/create-brand.dto';
 
+// Entities imports
+import { Brand } from '../../entities/brand.entity';
+
+@ApiTags('brands')
 @Controller('brands')
 export class BrandsController {
   constructor(private brandsService: BrandsService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'List of all brands',
+    type: [Brand],
+  })
   getAllBrands() {
     return this.brandsService.findAll();
   }
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Brand created successfully',
+    type: Brand,
+  })
+  @ApiBody({ type: CreateBrandDto })
   createBrand(@Body() payload: CreateBrandDto) {
     return this.brandsService.create(payload);
   }
 
   @Get(':id')
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'Brand details', type: Brand })
   getBrand(@Param('id', ParseIntPipe) id: number) {
     return this.brandsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({
+    status: 200,
+    description: 'Brand updated successfully',
+    type: Brand,
+  })
+  @ApiBody({ type: UpdateBrandDto })
   updateBrand(
     @Param('id', ParseIntPipe) id: number,
     @Body() payload: UpdateBrandDto,
@@ -45,6 +70,8 @@ export class BrandsController {
   }
 
   @Delete(':id')
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'Brand deleted successfully' })
   deleteBrand(@Param('id', ParseIntPipe) id: number) {
     return this.brandsService.delete(id);
   }
--- src/modules/products/dtos/brands/create-brand.dto.ts
+++ src/modules/products/dtos/brands/create-brand.dto.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateBrandDto {
+  @ApiProperty({
+    description: 'Name of the brand',
+    minLength: 3,
+    maxLength: 30,
+    example: 'Example Brand',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   @MaxLength(30)
   readonly name: string;
 
+  @ApiProperty({
+    description: 'Description of the brand',
+    minLength: 15,
+    maxLength: 255,
+    example: 'This is an example brand description',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(15)
