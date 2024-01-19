53312c9b8e0bf2eb9b377b79d9ba403fbd0d4fde
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 12:28:58 -0500

    feat: Add validation decorators to 'create-brand.dto.ts'
    - Applied the '@IsString', '@IsNotEmpty()', '@MinLength(3)', and '@MaxLength(30)' decorators to the 'name' property.
    - Added the '@IsString()', '@IsNotEmpty()', '@MinLength(15)', and '@MaxLength(255)' decorators to the 'description' property.
    - Changes made in the 'create-brand.dto.ts' file for property validations.

--- src/modules/products/dtos/brands/create-brand.dto.ts
+++ src/modules/products/dtos/brands/create-brand.dto.ts
@@ -1,4 +1,15 @@
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+
 export class CreateBrandDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(30)
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(15)
+  @MaxLength(255)
   readonly description: string;
 }
