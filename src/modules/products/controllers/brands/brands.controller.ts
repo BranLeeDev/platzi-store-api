f1c8c6e0d65870352f51d93b25d661afbe7ae0b2
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 14:27:55 -0500

    feat: Implement complete CRUD operations for 'brands'
    Added methods for GET, POST, PATCH, and DELETE in both services and controllers for 'brands'

--- src/modules/products/controllers/brands/brands.controller.ts
+++ src/modules/products/controllers/brands/brands.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+// NestJS modules
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+// Service imports
+import { BrandsService } from '../../services/brands/brands.service';
+
+// Dto imports
+import { UpdateBrandDto } from '../../dtos/brands/update-brand.dto';
+import { CreateBrandDto } from '../../dtos/brands/create-brand.dto';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  constructor(private brandsService: BrandsService) {}
+
+  @Get()
+  getAllBrands() {
+    return this.brandsService.findAll();
+  }
+
+  @Post()
+  createBrand(@Body() payload: CreateBrandDto) {
+    return this.brandsService.create(payload);
+  }
+
+  @Get(':id')
+  getBrand(@Param('id', ParseIntPipe) id: number) {
+    return this.brandsService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateBrand(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateBrandDto,
+  ) {
+    return this.brandsService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteBrand(@Param('id', ParseIntPipe) id: number) {
+    return this.brandsService.delete(id);
+  }
+}
--- src/modules/products/services/brands/brands.service.ts
+++ src/modules/products/services/brands/brands.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+// NestJS modules
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+// Third-party libraries
+import { Repository } from 'typeorm';
+
+// Entity imports
+import { Brand } from '../../entities/brand.entity';
+
+// Dto imports
+import { CreateBrandDto } from '../../dtos/brands/create-brand.dto';
+import { UpdateBrandDto } from '../../dtos/brands/update-brand.dto';
 
 @Injectable()
-export class BrandsService {}
+export class BrandsService {
+  constructor(
+    @InjectRepository(Brand) private readonly brandsRepo: Repository<Brand>,
+  ) {}
+
+  findAll() {
+    return this.brandsRepo.find();
+  }
+
+  findOne(id: number) {
+    const brand = this.brandsRepo.findOneBy({ id });
+    if (!brand) throw new NotFoundException(`Brand #${id} not Found`);
+    return brand;
+  }
+
+  async create(payload: CreateBrandDto) {
+    const newBrand = this.brandsRepo.create(payload);
+    await this.brandsRepo.save(newBrand);
+
+    return {
+      message: 'Brand created successfully',
+      data: newBrand,
+    };
+  }
+
+  async update(id: number, payload: UpdateBrandDto) {
+    const brand = await this.findOne(id);
+    this.brandsRepo.merge(brand, payload);
+    const updateResult = await this.brandsRepo.save(brand);
+
+    return {
+      message: 'Brand updated successfully',
+      data: updateResult,
+    };
+  }
+
+  async delete(id: number) {
+    const deleteResult = await this.findOne(id);
+    await this.brandsRepo.delete(id);
+
+    return {
+      message: 'Brand deleted successfully',
+      data: deleteResult,
+    };
+  }
+}
