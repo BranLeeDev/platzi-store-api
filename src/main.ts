2077a7bd1f3c895d56b4a2f246b515e33ff3848c
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 06:42:08 -0500

    feat: Applied CORS and Helmet middleware in main.ts
    Utilized 'enableCors()' and 'use(helmet())' on the 'app' constant. Imported 'helmet' library for these changes in the 'main.ts' file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+// NestJS modules
 import { NestFactory } from '@nestjs/core';
+
+// Third-party libraries
+import helmet from 'helmet';
+
+// Module imports
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
