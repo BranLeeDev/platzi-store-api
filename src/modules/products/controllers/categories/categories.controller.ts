46ca4cbe20d298b9d26bc55bceaf8bdd9a4a8021
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 16:14:14 -0500

    feat: Implement full CRUD operations for 'categories'
    Added methods for GET, POST, PATCH, and DELETE in both services and controllers for 'categories'

--- src/modules/products/controllers/categories/categories.controller.ts
+++ src/modules/products/controllers/categories/categories.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+// NestJS modules
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+// Service imports
+import { CategoriesService } from '../../services/categories/categories.service';
+
+// Dto imports
+import { CreateCategoryDto } from '../../dtos/categories/create-category.dto';
+import { UpdateCategoryDto } from '../../dtos/categories/update-category.dto';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  getAllCategories() {
+    return this.categoriesService.findAll();
+  }
+
+  @Post()
+  createCategory(@Body() payload: CreateCategoryDto) {
+    return this.categoriesService.create(payload);
+  }
+
+  @Get(':id')
+  getCategory(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateCategory(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteCategory(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.delete(id);
+  }
+}
--- src/modules/products/services/categories/categories.service.ts
+++ src/modules/products/services/categories/categories.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+// NestJS modules
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+// Third-party libraries
+import { Repository } from 'typeorm';
+
+// Entity imports
+import { Category } from '../../entities/category.entity';
+
+// DTO imports
+import { UpdateCategoryDto } from '../../dtos/categories/update-category.dto';
+import { CreateCategoryDto } from '../../dtos/categories/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoriesRepo: Repository<Category>,
+  ) {}
+
+  findAll() {
+    return this.categoriesRepo.find();
+  }
+
+  findOne(id: number) {
+    const category = this.categoriesRepo.findOneBy({ id });
+    if (!category) throw new NotFoundException(`Category #${id} not Found`);
+    return category;
+  }
+
+  async create(payload: CreateCategoryDto) {
+    const newCategory = this.categoriesRepo.create(payload);
+    await this.categoriesRepo.save(newCategory);
+
+    return {
+      message: 'Category created successfully',
+      data: newCategory,
+    };
+  }
+
+  async update(id: number, payload: UpdateCategoryDto) {
+    const category = await this.findOne(id);
+    this.categoriesRepo.merge(category, payload);
+    const updateResult = await this.categoriesRepo.save(category);
+
+    return {
+      message: 'Category updated successfully',
+      data: updateResult,
+    };
+  }
+
+  async delete(id: number) {
+    const deleteResult = await this.findOne(id);
+    await this.categoriesRepo.delete(id);
+
+    return {
+      message: 'Category deleted successfully',
+      data: deleteResult,
+    };
+  }
+}
