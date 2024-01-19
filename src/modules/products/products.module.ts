0d7c45717630779eb3ad29fcf20bb3c017aecd5e
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 15:02:56 -0500

    feat: Initialize 'categories' service and controller using NestJS CLI
    - Created 'categories.service.ts' and 'categories.controller.ts' in the 'controllers' and 'services', respectively.
    - Added 'create-category.dto.ts' and 'update.category.dto.ts' to the 'categories' folder within the 'dtos' directory.
    - Introduced 'categories.entity.ts' to the 'entities' folder.
    - DTOs are currently empty, and the entity implementation is pending.

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -4,16 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Controller imports
 import { BrandsController } from './controllers/brands/brands.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
 
 // Service imports
 import { BrandsService } from './services/brands/brands.service';
+import { CategoriesService } from './services/categories/categories.service';
 
 // Entity imports
 import { Brand } from './entities/brand.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Brand])],
-  controllers: [BrandsController],
-  providers: [BrandsService],
+  controllers: [BrandsController, CategoriesController],
+  providers: [BrandsService, CategoriesService],
 })
 export class ProductsModule {}
