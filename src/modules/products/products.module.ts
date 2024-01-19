846f4f83f292bae067aa50871873774fbd11a06a
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 08:36:54 -0500

    feat: Created 'brands' service and controller using NestJS CLI
    - Added 'brands' module to the 'products' module.
    - Created 'services' and 'controllers' folders with 'brands.service.ts' and 'brands.controller.ts'.
    - Added 'dtos' directory with 'create-brand.dto.ts' and 'update-brand.dto.ts' in the 'brands' folder.
    - Created 'entities' folder with 'brand.entity.ts'.
    - DTOs are currently empty, and the entity is pending implementation.

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,4 +1,14 @@
+// NestJS modules
 import { Module } from '@nestjs/common';
 
-@Module({})
+// Controller imports
+import { BrandsController } from './controllers/brands/brands.controller';
+
+// Service imports
+import { BrandsService } from './services/brands/brands.service';
+
+@Module({
+  controllers: [BrandsController],
+  providers: [BrandsService],
+})
 export class ProductsModule {}
