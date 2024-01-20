42c3f8e7e7ed044fffe53a54162610e4d91142b6
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-20 14:36:55 -0500

    fix: Added missing '@Entity({ name: 'products' })' decorator to the 'Product' class or entity
    - Previously, the '@Entity({ name: 'products' })' decorator was absent in the 'Product' class or entity.
    - This issue has been resolved in the 'product.entity.ts' file, which stores the 'Product' entity.

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,10 +1,12 @@
 import {
   Column,
   CreateDateColumn,
+  Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 
+@Entity({ name: 'products' })
 export class Product {
   @PrimaryGeneratedColumn()
   id: number;
