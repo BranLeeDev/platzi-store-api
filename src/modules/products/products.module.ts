63e5b178725bc3a25eee65076b7b4a200f34d1c7
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-19 19:23:01 -0500

    feat: Initialized 'products' service and controller using NestJS CLI
    - Created 'products.service.ts' and 'products.controller.ts' in the 'controllers' and 'services' directories, respectively.
    - Added 'create-product.dto.ts' and 'update.product.dto.ts' to the 'products' folder within the 'dtos' directory.
    - Introduced 'product.entity.ts' to the 'entities' folder.
    - The DTOs were left empty, and the entity implementation was pending.

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 // Controller imports
 import { BrandsController } from './controllers/brands/brands.controller';
 import { CategoriesController } from './controllers/categories/categories.controller';
+import { ProductsController } from './controllers/products/products.controller';
 
 // Service imports
 import { BrandsService } from './services/brands/brands.service';
 import { CategoriesService } from './services/categories/categories.service';
+import { ProductsService } from './services/products/products.service';
 
 // Entity imports
 import { Brand } from './entities/brand.entity';
@@ -16,7 +18,7 @@ import { Category } from './entities/category.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Brand, Category])],
-  controllers: [BrandsController, CategoriesController],
-  providers: [BrandsService, CategoriesService],
+  controllers: [BrandsController, CategoriesController, ProductsController],
+  providers: [BrandsService, CategoriesService, ProductsService],
 })
 export class ProductsModule {}
