67f596949b62dce28f3c10b5fe358c79333eef2c
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-20 14:37:53 -0500

    feat: Implement full CRUD operations for 'products'
    Added methods for GET, POST, PATCH, and DELETE in both services and controllers for 'products'

--- src/modules/products/controllers/products/products.controller.ts
+++ src/modules/products/controllers/products/products.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ProductsService } from '../../services/products/products.service';
+import { CreateProductDto } from '../../dtos/products/create-product.dto';
+import { UpdateProductDto } from '../../dtos/products/update-product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.findAll();
+  }
+
+  @Post()
+  createProduct(@Body() payload: CreateProductDto) {
+    return this.productsService.create(payload);
+  }
+
+  @Get(':id')
+  getProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateProductDto,
+  ) {
+    return this.productsService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.delete(id);
+  }
+}
--- src/modules/products/services/products/products.service.ts
+++ src/modules/products/services/products/products.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from '../../entities/product.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from '../../dtos/products/create-product.dto';
+import { UpdateProductDto } from '../../dtos/products/update-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepo: Repository<Product>,
+  ) {}
+
+  findAll() {
+    return this.productsRepo.find();
+  }
+
+  async findOne(id: number) {
+    const product = await this.productsRepo.findOneBy({ id });
+    if (!product) throw new NotFoundException(`Product #${id} not Found`);
+    return product;
+  }
+
+  async create(payload: CreateProductDto) {
+    const newProduct = this.productsRepo.create(payload);
+    await this.productsRepo.save(newProduct);
+
+    return {
+      message: 'Product created successfully',
+      data: newProduct,
+    };
+  }
+
+  async update(id: number, payload: UpdateProductDto) {
+    const productFound = await this.findOne(id);
+    this.productsRepo.merge(productFound, payload);
+    const updatedResult = await this.productsRepo.save(productFound);
+
+    return {
+      message: 'Product updated successfully',
+      data: updatedResult,
+    };
+  }
+
+  async delete(id: number) {
+    const deletedProduct = await this.findOne(id);
+    await this.productsRepo.delete(deletedProduct);
+
+    return {
+      message: 'Product deleted successfully',
+      data: deletedProduct,
+    };
+  }
+}
