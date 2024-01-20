bd87198d6ec2cc30c2f4e14e88891cfa13b39311
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-20 14:48:45 -0500

    docs: Add Swagger annotations for 'products' controller and create-dto

--- src/modules/products/controllers/products/products.controller.ts
+++ src/modules/products/controllers/products/products.controller.ts
@@ -11,27 +11,49 @@ import {
 import { ProductsService } from '../../services/products/products.service';
 import { CreateProductDto } from '../../dtos/products/create-product.dto';
 import { UpdateProductDto } from '../../dtos/products/update-product.dto';
+import { ApiBody, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Product } from '../../entities/product.entity';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'List of all products',
+    type: [Product],
+  })
   getAllProducts() {
     return this.productsService.findAll();
   }
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+    type: Product,
+  })
+  @ApiBody({ type: CreateProductDto })
   createProduct(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
   }
 
   @Get(':id')
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'Product details', type: Product })
   getProduct(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({
+    status: 200,
+    description: 'Product updated successfully',
+    type: Product,
+  })
   updateProduct(
     @Param('id', ParseIntPipe) id: number,
     @Body() payload: UpdateProductDto,
@@ -40,6 +62,8 @@ export class ProductsController {
   }
 
   @Delete(':id')
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'Product deleted successfully' })
   deleteProduct(@Param('id', ParseIntPipe) id: number) {
     return this.productsService.delete(id);
   }
--- src/modules/products/dtos/products/create-product.dto.ts
+++ src/modules/products/dtos/products/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsInt,
   IsNotEmpty,
@@ -9,6 +10,12 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Name of the product',
+    minLength: 3,
+    maxLength: 50,
+    example: 'Example product',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
@@ -19,12 +26,27 @@ export class CreateProductDto {
   @IsNotEmpty()
   @MinLength(10)
   @MaxLength(1500)
+  @ApiProperty({
+    description: 'Description on the product',
+    minLength: 10,
+    maxLength: 1500,
+    example: 'This is an example brand description',
+  })
   readonly description: string;
 
+  @ApiProperty({
+    description: 'Price of the product',
+    example: 123.45,
+  })
   @IsNumber()
   @IsPositive()
   readonly price: number;
 
+  @ApiProperty({
+    description: 'Stock of the product',
+    minimum: 1,
+    example: 100,
+  })
   @IsNumber()
   @IsPositive()
   @IsInt()
