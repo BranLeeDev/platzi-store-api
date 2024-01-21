bd6dd3298ece5291990828f7785d76ab4b9b0698
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-21 13:31:59 -0500

    feat: Implement complete CRUD for 'users'
    Added GET, POST, PATCH, and DELETE methods for full CRUD functionality on 'users'

--- src/modules/users/controllers/users/users.controller.ts
+++ src/modules/users/controllers/users/users.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+// NestJS modules
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+// Service imports
+import { UsersService } from '../../services/users/users.service';
+
+// DTO imports
+import { CreateUserDto } from '../../dtos/users/create-user.dto';
+import { UpdateUserDto } from '../../dtos/users/update-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  createUser(@Body() payload: CreateUserDto) {
+    return this.usersService.create(payload);
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateUserDto,
+  ) {
+    return this.usersService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
+  }
+}
--- src/modules/users/services/users/users.service.ts
+++ src/modules/users/services/users/users.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+// NestJS modules
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+// Third-party libraries
+import { Repository } from 'typeorm';
+
+// DTO imports
+import { CreateUserDto } from '../../dtos/users/create-user.dto';
+import { UpdateUserDto } from '../../dtos/users/update-user.dto';
+
+// Entity imports
+import { User } from '../../entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+
+  findAll() {
+    return this.userRepo.find();
+  }
+
+  async findOne(id: number) {
+    const user = await this.userRepo.findOneBy({ id });
+    if (!user) throw new NotFoundException(`User #${id} not Found`);
+    return user;
+  }
+
+  async create(payload: CreateUserDto) {
+    const newUser = this.userRepo.create(payload);
+    await this.userRepo.save(newUser);
+
+    return {
+      message: 'User created successfully',
+      data: newUser,
+    };
+  }
+
+  async update(id: number, payload: UpdateUserDto) {
+    const userFound = await this.findOne(id);
+    this.userRepo.merge(userFound, payload);
+    const updatedResult = await this.userRepo.save(userFound);
+
+    return {
+      message: 'User updated successfully',
+      data: updatedResult,
+    };
+  }
+
+  async delete(id: number) {
+    const deletedUser = await this.findOne(id);
+    await this.userRepo.delete(id);
+
+    return {
+      message: 'User deleted successfully',
+      data: deletedUser,
+    };
+  }
+}
