e3e575173c6e8795ed6197cac685ceaf5a0ff888
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-21 14:17:33 -0500

    docs: Add Swagger annotations for 'users' controller and create-dto

--- src/modules/users/controllers/users/users.controller.ts
+++ src/modules/users/controllers/users/users.controller.ts
@@ -16,27 +16,71 @@ import { UsersService } from '../../services/users/users.service';
 // DTO imports
 import { CreateUserDto } from '../../dtos/users/create-user.dto';
 import { UpdateUserDto } from '../../dtos/users/update-user.dto';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { User } from '../../entities/user.entity';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all users',
+    description: 'Retrieve a list of all users',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of all users',
+    type: [User],
+  })
   getAllUsers() {
     return this.usersService.findAll();
   }
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a user',
+    description: 'Create a new user',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'User created successfully',
+    type: User,
+  })
+  @ApiBody({ type: CreateUserDto })
   createUser(@Body() payload: CreateUserDto) {
     return this.usersService.create(payload);
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get user by ID',
+    description: 'Retrieve details of a specific user by ID',
+  })
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({ status: 200, description: 'User details', type: User })
   getUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update user by ID',
+    description: 'Update details of a specific user by ID',
+  })
+  @ApiParam({ name: 'id', type: 'number' })
+  @ApiResponse({
+    status: 200,
+    description: 'User updated successfully',
+    type: User,
+  })
   updateUser(
     @Param('id', ParseIntPipe) id: number,
     @Body() payload: UpdateUserDto,
@@ -45,6 +89,16 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete user by ID',
+    description: 'Delete a specific user by ID',
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'number',
+    description: 'ID of the user',
+  })
+  @ApiResponse({ status: 200, description: 'User deleted successfully' })
   deleteUser(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.delete(id);
   }
--- src/modules/users/dtos/users/create-user.dto.ts
+++ src/modules/users/dtos/users/create-user.dto.ts
@@ -8,14 +8,29 @@ import {
   MinLength,
 } from 'class-validator';
 import { ROLES } from '../../types/enums';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Username for the new user.',
+    minLength: 3,
+    maxLength: 20,
+    example: 'branleedev',
+    uniqueItems: true,
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   @MaxLength(20)
   username: string;
 
+  @ApiProperty({
+    description: 'Email address for the new user.',
+    minLength: 11,
+    maxLength: 320,
+    example: 'brandonaguerodeveloper@gmail.com',
+    uniqueItems: true,
+  })
   @IsString()
   @IsNotEmpty()
   @IsEmail()
@@ -23,12 +38,24 @@ export class CreateUserDto {
   @MaxLength(320)
   email: string;
 
+  @ApiProperty({
+    description: 'Password for the new user.',
+    minLength: 8,
+    maxLength: 100,
+    example: 'W1RS}4u0',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(8)
   @MaxLength(100)
   password: string;
 
+  @ApiProperty({
+    description: 'Role of the new user (optional).',
+    enum: ROLES,
+    required: false,
+    example: 'admin',
+  })
   @IsString()
   @IsNotEmpty()
   @IsOptional()
