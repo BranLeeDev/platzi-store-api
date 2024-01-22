005813c8cdefa45c29e7cb587c5affa5442d9d64
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-21 19:49:23 -0500

    feat: Initialize 'customers' controller, service, DTO, and entity
    - Generated 'customers' controller and service using NestJS CLI.
    - Manually created an empty DTO and entity for 'customers'.
    - Executed actions within the 'users' module.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -4,16 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 // Controller imports
 import { UsersController } from './controllers/users/users.controller';
+import { CustomersController } from './controllers/customers/customers.controller';
 
 // Service imports
 import { UsersService } from './services/users/users.service';
+import { CustomersService } from './services/customers/customers.service';
 
 // Entity imports
 import { User } from './entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
-  providers: [UsersService],
+  controllers: [UsersController, CustomersController],
+  providers: [UsersService, CustomersService],
 })
 export class UsersModule {}
