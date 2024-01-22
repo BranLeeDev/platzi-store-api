3c7bddecc5cda624d122c98f67edefe5dbdf643c
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-22 12:44:59 -0500

    feat: Implement complete CRUD for 'customers'
    Added GET, POST, PATCH, and DELETE methods for full CRUD functionality on 'customers'

--- src/modules/users/controllers/customers/customers.controller.ts
+++ src/modules/users/controllers/customers/customers.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+// NestJS modules
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+// Service imports
+import { CustomersService } from '../../services/customers/customers.service';
+
+// DTO imports
+import { CreateCustomerDto } from '../../dtos/customers/create-customer.dto';
+import { UpdateCustomerDto } from '../../dtos/customers/update-customer.dto';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  constructor(private readonly customersService: CustomersService) {}
+
+  @Get()
+  getAllCustomers() {
+    return this.customersService.findAll();
+  }
+
+  @Post()
+  createCustomer(@Body() payload: CreateCustomerDto) {
+    return this.customersService.create(payload);
+  }
+
+  @Get(':id')
+  getCustomer(@Param('id', ParseIntPipe) id: number) {
+    return this.customersService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateCustomer(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateCustomerDto,
+  ) {
+    return this.customersService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteCustomer(@Param('id', ParseIntPipe) id: number) {
+    return this.customersService.delete(id);
+  }
+}
--- src/modules/users/services/customers/customers.service.ts
+++ src/modules/users/services/customers/customers.service.ts
@@ -1,4 +1,61 @@
-import { Injectable } from '@nestjs/common';
+// NestJS modules
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+// Third-party libraries
+import { Repository } from 'typeorm';
+
+// Entity imports
+import { Customer } from '../../entities/customer.entity';
+
+// DTO imports
+import { CreateCustomerDto } from '../../dtos/customers/create-customer.dto';
+import { UpdateCustomerDto } from '../../dtos/customers/update-customer.dto';
 
 @Injectable()
-export class CustomersService {}
+export class CustomersService {
+  constructor(
+    @InjectRepository(Customer)
+    private readonly customerRepo: Repository<Customer>,
+  ) {}
+
+  findAll() {
+    return this.customerRepo.find();
+  }
+
+  async findOne(id: number) {
+    const customer = await this.customerRepo.findOneBy({ id });
+    if (!customer) throw new NotFoundException(`Customer #${id} not Found`);
+    return customer;
+  }
+
+  async create(payload: CreateCustomerDto) {
+    const newCustomer = this.customerRepo.create(payload);
+    await this.customerRepo.save(newCustomer);
+    return {
+      message: 'Customer created successfully',
+      data: newCustomer,
+    };
+  }
+
+  async update(id: number, payload: UpdateCustomerDto) {
+    const customerFound = await this.findOne(id);
+    this.customerRepo.merge(customerFound, payload);
+    const updatedResult = await this.customerRepo.save(customerFound);
+
+    return {
+      message: 'Customer updated successfully',
+      data: updatedResult,
+    };
+  }
+
+  async delete(id: number) {
+    const deletedCustomer = await this.findOne(id);
+    await this.customerRepo.delete(id);
+
+    return {
+      message: 'Customer deleted successfully',
+      data: deletedCustomer,
+    };
+  }
+}
