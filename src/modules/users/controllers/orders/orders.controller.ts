5eebf2c46b8124ce8f5eaf72715b2a293fba1788
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-22 14:33:04 -0500

    feat: Implement complete CRUD for 'orders'
    Added GET, POST, PATCH, and DELETE methods for full CRUD functionality on 'orders'

--- src/modules/users/controllers/orders/orders.controller.ts
+++ src/modules/users/controllers/orders/orders.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+// NestJS modules
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+// Service imports
+import { OrdersService } from '../../services/orders/orders.service';
+
+// DTO imports
+import { CreateOrderDto } from '../../dtos/orders/create-order.dto';
+import { UpdateOrderDto } from '../../dtos/orders/update-order.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Get()
+  getAllOrders() {
+    return this.ordersService.findAll();
+  }
+
+  @Post()
+  createOrder(@Body() payload: CreateOrderDto) {
+    return this.ordersService.create(payload);
+  }
+
+  @Get(':id')
+  getOrder(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @Patch(':id')
+  updateOrder(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateOrderDto,
+  ) {
+    return this.ordersService.update(id, payload);
+  }
+
+  @Delete(':id')
+  deleteOrder(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.delete(id);
+  }
+}
--- src/modules/users/services/orders/orders.service.ts
+++ src/modules/users/services/orders/orders.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+// NestJS modules
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+// Third-party libraries
+import { Repository } from 'typeorm';
+
+// Entity imports
+import { Order } from '../../entities/order.entity';
+
+// DTO imports
+import { CreateOrderDto } from '../../dtos/orders/create-order.dto';
+import { UpdateOrderDto } from '../../dtos/orders/update-order.dto';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @InjectRepository(Order) private readonly orderRepo: Repository<Order>,
+  ) {}
+
+  findAll() {
+    return this.orderRepo.find();
+  }
+
+  async findOne(id: number) {
+    const order = await this.orderRepo.findOneBy({ id });
+    if (!order) throw new NotFoundException(`Order #${id} not Found`);
+    return order;
+  }
+
+  async create(payload: CreateOrderDto) {
+    const newOrder = this.orderRepo.create(payload);
+    await this.orderRepo.save(newOrder);
+    return {
+      message: 'Order created successfully',
+      data: newOrder,
+    };
+  }
+
+  async update(id: number, payload: UpdateOrderDto) {
+    const orderFound = await this.findOne(id);
+    this.orderRepo.merge(orderFound, payload);
+    const updatedResult = await this.orderRepo.save(orderFound);
+
+    return {
+      message: 'Order updated successfully',
+      data: updatedResult,
+    };
+  }
+
+  async delete(id: number) {
+    const deletedResult = await this.findOne(id);
+    await this.orderRepo.delete(id);
+
+    return {
+      message: 'Order deleted successfully',
+      data: deletedResult,
+    };
+  }
+}
