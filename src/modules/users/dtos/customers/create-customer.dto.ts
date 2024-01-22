60fc81f71dd222653a7f4b5ffb21d3eada29bb95
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-22 12:41:00 -0500

    refactor: Update decorators for 'dateOfBirth' attribute in 'CreateCustomerDto'
    - Modified decorators for the 'readonly dateOfBirth: Date;' attribute in 'create-customer.dto.ts'.
    - Removed '@IsString()' decorator.
    - Added the following decorators:
      - '@MinDate(new Date('1990-01-01'))'
      - '@MaxDate(() => { const dateString = new Date().toISOString().split('T')[0]; return new Date(dateString); })'

--- src/modules/users/dtos/customers/create-customer.dto.ts
+++ src/modules/users/dtos/customers/create-customer.dto.ts
@@ -1,12 +1,17 @@
+// Third-party libraries
 import {
   IsDate,
   IsEnum,
   IsNotEmpty,
   IsString,
+  MaxDate,
   MaxLength,
+  MinDate,
   MinLength,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+
+// Type imports
 import { GENDERS } from '../../types/enums';
 
 export class CreateCustomerDto {
@@ -27,9 +32,13 @@ export class CreateCustomerDto {
   @IsEnum(GENDERS)
   readonly gender: GENDERS;
 
-  @IsString()
   @IsNotEmpty()
   @IsDate()
+  @MinDate(new Date('1990-01-01'))
+  @MaxDate(() => {
+    const dateString = new Date().toISOString().split('T')[0];
+    return new Date(dateString);
+  })
   @Type(() => Date)
   readonly dateOfBirth: Date;
 }
