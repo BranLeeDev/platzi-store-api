2478230a0c6766864e2e70ad65792572e7ec6b96
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-22 14:07:57 -0500

    feat: Generate 'orders' service and controller with NestJS CLI
    - Created 'orders' service and controller using NestJS CLI.
    - Added 'order.entity.ts' file within the 'entities' folder, currently empty.
    - Introduced 'orders' folder within the 'dtos' folder.
    - Inside the 'orders' folder, added 'create-order.dto.ts' and 'update-order.dto.ts', both currently empty.

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 // Controller imports
 import { UsersController } from './controllers/users/users.controller';
 import { CustomersController } from './controllers/customers/customers.controller';
+import { OrdersController } from './controllers/orders/orders.controller';
 
 // Service imports
 import { UsersService } from './services/users/users.service';
 import { CustomersService } from './services/customers/customers.service';
+import { OrdersService } from './services/orders/orders.service';
 
 // Entity imports
 import { User } from './entities/user.entity';
@@ -16,7 +18,7 @@ import { Customer } from './entities/customer.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Customer])],
-  controllers: [UsersController, CustomersController],
-  providers: [UsersService, CustomersService],
+  controllers: [UsersController, CustomersController, OrdersController],
+  providers: [UsersService, CustomersService, OrdersService],
 })
 export class UsersModule {}
