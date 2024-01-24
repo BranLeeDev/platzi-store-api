45cc9c5ca4f8b4f7bca19e090b803248c606be46
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-23 20:58:21 -0500

    feat: Implement new logic in 'orders-products' service
    - Injected the repositories for 'Order', 'Product', and 'OrderProduct' into the constructor.
    - Modified the 'create' method to use 'findOne' instead of 'findOneBy' for 'order' and 'product'.
    - Created a new 'OrderProduct' instance, set its properties, and saved it using the repository.

--- src/modules/users/services/orders-products/orders-products.service.ts
+++ src/modules/users/services/orders-products/orders-products.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from '../../entities/order.entity';
+import { Repository } from 'typeorm';
+import { Product } from 'src/modules/products/entities/product.entity';
+import { OrderProduct } from '../../entities/order-product.entity';
+import { CreateOrderProductDto } from '../../dtos/orders-products/create-order-product.dto';
 
 @Injectable()
-export class OrdersProductsService {}
+export class OrdersProductsService {
+  constructor(
+    @InjectRepository(Order) private readonly orderRepo: Repository<Order>,
+    @InjectRepository(Product)
+    private readonly productRepo: Repository<Product>,
+    @InjectRepository(OrderProduct)
+    private readonly orderProductRepo: Repository<OrderProduct>,
+  ) {}
+
+  async create(payload: CreateOrderProductDto) {
+    const order = await this.orderRepo.findOneBy({ id: payload.orderId });
+    const product = await this.productRepo.findOneBy({ id: payload.productId });
+    const productsOrder = new OrderProduct();
+    productsOrder.order = order;
+    productsOrder.product = product;
+    productsOrder.quantity = payload.quantity;
+
+    return this.orderProductRepo.save(productsOrder);
+  }
+}
