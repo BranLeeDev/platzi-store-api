984893d1fdc2a5ccb5bec0ad2a55032fa0d0b4a5
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-23 20:51:22 -0500

    feat: Inject 'OrdersProductsService' into the constructor
    Added the 'OrdersProductsService' as a dependency in the constructor.

--- src/modules/users/controllers/orders-products/orders-products.controller.ts
+++ src/modules/users/controllers/orders-products/orders-products.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+// NestJS modules
+import { Body, Controller, Post } from '@nestjs/common';
+
+// Service imports
+import { OrdersProductsService } from '../../services/orders-products/orders-products.service';
+
+// DTO imports
+import { CreateOrderProductDto } from '../../dtos/orders-products/create-order-product.dto';
 
 @Controller('orders-products')
-export class OrdersProductsController {}
+export class OrdersProductsController {
+  constructor(private readonly ordersProductsService: OrdersProductsService) {}
+
+  @Post()
+  create(@Body() payload: CreateOrderProductDto) {
+    return this.ordersProductsService.create(payload);
+  }
+}
