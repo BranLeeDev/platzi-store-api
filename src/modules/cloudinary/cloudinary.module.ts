258db9ca68e550fe4a66e6cd7824bc946ddda7a4
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-26 13:07:52 -0500

    feat: Set up Cloudinary provider configuration
    - Configured the 'provide' for Cloudinary with the following settings:
      - Provide name: 'CLOUDINARY'
      - Dependencies: [registers.KEY]
      - Factory function uses the 'registers' service to retrieve Cloudinary configuration.
      - Cloudinary configuration is obtained from the 'registersService.cloudinary' object.
      - The Cloudinary configuration is used to set up the Cloudinary provider.

--- src/modules/cloudinary/cloudinary.module.ts
+++ src/modules/cloudinary/cloudinary.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ImagesService } from './services/images/images.service';
 import { ImagesController } from './controllers/images/images.controller';
+import registers from 'src/configs/registers';
+import { ConfigType } from '@nestjs/config';
+import { v2 as cloudinary } from 'cloudinary';
 
 @Module({
-  providers: [ImagesService],
   controllers: [ImagesController],
+  providers: [
+    ImagesService,
+    {
+      provide: 'CLOUDINARY',
+      inject: [registers.KEY],
+      useFactory: (registersService: ConfigType<typeof registers>) => {
+        const { cloudName, apiKey, apiSecret } = registersService.cloudinary;
+        return cloudinary.config({
+          cloud_name: cloudName,
+          api_key: apiKey,
+          api_secret: apiSecret,
+        });
+      },
+    },
+  ],
 })
 export class CloudinaryModule {}
