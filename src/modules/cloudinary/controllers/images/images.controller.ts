b043149489300534c46aa216131ce67f0afce6fc
Brandon <brandonaguerodeveloper@gmail.com>
2024-01-27 08:10:59 -0500

    fix: Added import statement for 'Express' in the service and controller for the 'images' table
    - Imported { Express } from 'express' to resolve an issue with the 'images' service and controller.

--- src/modules/cloudinary/controllers/images/images.controller.ts
+++ src/modules/cloudinary/controllers/images/images.controller.ts
@@ -14,6 +14,7 @@ import {
 } from '@nestjs/common';
 import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 // Entities
 import { Image } from '../../entities/image.entity';
--- src/modules/cloudinary/services/images/images.service.ts
+++ src/modules/cloudinary/services/images/images.service.ts
@@ -10,6 +10,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { v2 as cloudinary } from 'cloudinary';
 import * as streamifier from 'streamifier';
+import { Express } from 'express';
 
 // Types
 import { CloudinaryResponse } from '../../types/types';
